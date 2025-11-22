dc674a366d144e9ee10cf853b324eb7eb81ef2fe
StageDog <aksanajw845@gmail.com>
2025-11-22 16:23:29 +0800

    feat: 重定向首页

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -85,14 +85,7 @@ function App() {
   return (
     <SetupCheck>
       <Routes>
-        <Route
-          path='/'
-          element={
-            <Suspense fallback={<Loading></Loading>} key={location.pathname}>
-              <Home />
-            </Suspense>
-          }
-        />
+        <Route path='/' element={<AuthRedirect to='/console' />} />
         <Route
           path='/setup'
           element={
